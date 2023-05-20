a56db1fa1f006f8015f259f19a71ab4c72031388
sk0g <ani.mehta@mail.com>
2023-05-20 13:44:36 +1000

    Sword ability spawning

---
+++ Scenes/Abilities/SwordAbility/SwordAbility.cs
@@ -0,0 +1,8 @@
+using Godot;
+
+public partial class SwordAbility : Node2D
+{
+    public override void _Ready() { }
+
+    public override void _Process(double delta) { }
+}
---
+++ Scenes/Abilities/SwordAbility/SwordAbilityController.cs
@@ -0,0 +1,30 @@
+using System;
+using Godot;
+
+public partial class SwordAbilityController : Node
+{
+    Timer _abilityTimer;
+
+    [Export]
+    PackedScene _swordAbility;
+
+    public override void _Ready()
+    {
+        _abilityTimer = GetNode<Timer>("AbilityTimer");
+        _abilityTimer.Timeout += HandleAbilityTimer;
+    }
+
+    public override void _Process(double delta) { }
+
+    void HandleAbilityTimer()
+    {
+        var player = GetTree().GetFirstNodeInGroup("Player") as Player
+         ?? throw new Exception("E003: Ability did not find player");
+
+        var swordInstance = _swordAbility.Instantiate() as Node2D
+         ?? throw new Exception("E004: Ability could not instantiate");
+
+        swordInstance.GlobalPosition = player.GlobalPosition;
+        player.GetParent().AddChild(swordInstance);
+    }
+}
