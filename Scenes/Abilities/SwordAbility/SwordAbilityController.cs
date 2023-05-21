e9e0728f63b5dd5b7ee10d864008adcbdc23b4ba
sk0g <ani.mehta@mail.com>
2023-05-21 10:14:56 +1000

    Nearest target selection for SwordAbility

--- Scenes/Abilities/SwordAbility/SwordAbilityController.cs
+++ Scenes/Abilities/SwordAbility/SwordAbilityController.cs
@@ -1,9 +1,13 @@
 using System;
+using System.Linq;
 using Godot;
 
 public partial class SwordAbilityController : Node
 {
     Timer _abilityTimer;
+
+    [Export]
+    float _maxRange = 200f;
 
     [Export]
     PackedScene _swordAbility;
@@ -21,10 +25,25 @@ public partial class SwordAbilityController : Node
         var player = GetTree().GetFirstNodeInGroup("player") as Player
          ?? throw new Exception("E003: SwordAbility did not find player");
 
+        var nearestEnemy = GetTree()
+            .GetNodesInGroup("enemy")
+            .OfType<Node2D>()
+            .Select(enemy => new Tuple<Node2D, float>(enemy,
+                enemy.GlobalPosition.DistanceSquaredTo(player.GlobalPosition)))
+            .Where(enemyAndDistance => enemyAndDistance.Item2 < Mathf.Pow(_maxRange, 2))
+            .MinBy(enemyAndDistance => enemyAndDistance.Item2)
+            ?.Item1;
+
+        if (nearestEnemy is null)
+        {
+            GD.Print("SwordAbility: No nearby enemy found to attack");
+            return;
+        }
+
         var swordInstance = _swordAbility.Instantiate() as Node2D
          ?? throw new Exception("E004: SwordAbility could not instantiate");
 
-        swordInstance.GlobalPosition = player.GlobalPosition;
+        swordInstance.GlobalPosition = nearestEnemy.GlobalPosition;
         player.GetParent().AddChild(swordInstance);
     }
 }
